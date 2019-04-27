b48d3207a05df8c97070a8d8d3197797d49277e2
Tomasz Sieczko <tomasz.sieczko@nevada.unr.edu>
2019-04-26 22:23:21 -0700

    added permissions to manifest

--- android/src/main/java/com/reactlibrary/T12CapstoneNativeCodeModule.java
+++ android/src/main/java/com/reactlibrary/T12CapstoneNativeCodeModule.java
@@ -6,23 +6,38 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Promise;
 
+import android.net.wifi.WifiManager;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+
 public class T12CapstoneNativeCodeModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+	WifiManager wifi;
 
-  public T12CapstoneNativeCodeModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+	public T12CapstoneNativeCodeModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+		wifi = (WifiManager) reactContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+	}
 
-  @Override
-  public String getName() {
-    return "T12CapstoneNativeCode";
-  }
+	@Override
+	public String getName() {
+		return "T12CapstoneNativeCode";
+	}
 
 	@ReactMethod
-	public void returnSomeStuff(Promise promise) {
-		String result = "Yeah";
+	public void sayHey(Promise promise) {
+		String result = "hey";
 		promise.resolve(result);
 	}
+
+	@ReactMethod
+	public void getSSID(Promise promise) {
+		try {
+			WifiInfo info = wifi.getConnectionInfo();
+			promise.resolve(info.getSSID());
+		} catch(Exception e) {
+			promise.reject(e);
+		}
+	}
+
 }
